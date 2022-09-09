16edf059c0938d0b615f9456149d1ddc0321fa82
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2022-09-09 11:30:17 +0530

    import java.util.Scanner; class helloworld {     public static void main(String args[])     {         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();
            System.out.println(s);
            
            sc.close();
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class helloworld
+{
+    public static void main(String args[])
+    {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        
+        System.out.println(s);
+        
+        sc.close();
+    }
+}
