67665853e2922a8a13c4acb8750879ec7e03564f
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2022-09-29 23:20:34 +0530

    import java.util.Scanner; class array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0,k;         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]==k)             {                 c=i;             }         }         if(c>0)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0,k;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]==k)
+            {
+                c=i;
+            }
+        }
+        if(c>0)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
