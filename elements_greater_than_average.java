e69dd792b614441fa4fdac717dcfca36d62f1a5e
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2022-09-29 22:27:13 +0530

    import java.util.Scanner; class element {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,avg=0,c=0;         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             s+=x[i];         }         avg=s/n;         for(i=0;i<n;i++)         {             if(x[i]>=avg)             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class element
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,avg=0,c=0;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            s+=x[i];
+        }
+        avg=s/n;
+        for(i=0;i<n;i++)
+        {
+            if(x[i]>=avg)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
