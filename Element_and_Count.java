0e865038f70f1f5808274d54e54630ad6c7b7b82
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2022-12-07 17:46:27 +0530

    import java.util.Scanner; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,f=0,ind=0,c=0;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int b[]=new int[n];         for(i=0;i<n;i++)         {             f=0;             for(j=0;j<i;j++)             {                 if(arr[i]==arr[j])                 {                     f=1;                     break;                 }             }             if(f==0)             {                 b[ind++]=arr[i];             }         }         for(i=0;i<ind;i++)         {             c=0;             for(j=0;j<n;j++)             {                 if(b[i]==arr[j])                 {                     c+=1;                 }             }             System.out.print(b[i]+" "+c+" ");         }     } }

---
+++ Element_and_Count.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,f=0,ind=0,c=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int b[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            f=0;
+            for(j=0;j<i;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    f=1;
+                    break;
+                }
+            }
+            if(f==0)
+            {
+                b[ind++]=arr[i];
+            }
+        }
+        for(i=0;i<ind;i++)
+        {
+            c=0;
+            for(j=0;j<n;j++)
+            {
+                if(b[i]==arr[j])
+                {
+                    c+=1;
+                }
+            }
+            System.out.print(b[i]+" "+c+" ");
+        }
+    }
+}
